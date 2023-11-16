de930b32887baa14f0e771a84b5afc0af64e9703
yadicod <yadiadi2020@gmail.com>
2023-11-16 10:30:07 +0700

    laravel 3

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LihatNilaiController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\PagenotController;
+use App\Http\Controllers\KartuController;
+use App\Http\Controllers\JenisProdukController;
+use App\Http\Controllers\ProdukController;
+use App\Http\Controllers\PelangganController;
 
 /*
 |--------------------------------------------------------------------------
@@ -41,5 +45,22 @@ Route::get('/daftarnilai', function(){
 //routing manggil dari class controller
 Route::get('/datamahasiswa', [LihatNilaiController::class, 'dataMahasiswa']);
 
+Route::prefix('admin')->group(function(){
 Route::get('/dashboard', [DashboardController::class, 'index']);
+
+//contoh pemanggilan secara satu persatu function menggunakan get,put, update, delete
 Route::get('/notfound', [PagenotController::class, 'index']);
+
+//memanggil seluruh fungsi atau function menggunakan resource
+Route::resource('kartu', KartuController::class);
+
+//memanggil fungsi satu persatu
+Route::get('/jenis_produk', [JenisProdukController::class, 'index']);
+
+
+//routing untuk table produk
+Route::get('/produk', [ProdukController::class, 'index']);
+
+Route::resource('pelanggan', PelangganController::class);
+
+});
